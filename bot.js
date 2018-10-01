c0e34f2b21e4992d513ef2d349d7d6ee7af6d1e8
Mitchell Dzurisin <madlab96@gmail.com>
2018-10-01 14:12:16 -0500

    more commands

--- bot.js
+++ bot.js
@@ -8,24 +8,18 @@ logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
     colorize: true
 });
-// logger.level = 'debug';
-// Initialize Discord Bot
-// var bot = new Discord.Client({
-//    token: auth.token,
-//    autorun: true
-// });
+logger.level = 'debug';
+
 client.on('ready', function (evt) {
     logger.info('Connected');
 });
 client.on('message', function (msg) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    // logger.info(user + " " + userID + " sent message: " + message);
-
     if (msg.author.username === "Jasper") {
         msg.channel.send('@Jasper#9254 *shhh*');
         msg.delete();
-    } else if (msg.content.substring(0, 1) == '!') {
+    } 
+    else
+    if (msg.content.substring(0, 1) == '!') {
         var args = msg.content.substring(1).split(' ');
         var cmd = args[0];
        
@@ -36,7 +30,12 @@ client.on('message', function (msg) {
             case 'ping':
                 msg.channel.send('Pong!')
                     .then(msg => logger.info(`Sent message replying to ${msg.author.id}.`));
-            break;
+                break;
+            case 'source':
+                msg.channel.send(`${msg.author} https://github.com/MADLAB96/suffer-bot`);
+                break;
+            default:
+                msg.channel.send('Unknown Command :b:ussy');
             // Just add any case commands if you want to..
          }
     } 
