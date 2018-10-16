7c152f0f1886ccf5a556548b909e6c8cf717d3b2
Mitchell Dzurisin <madlab96@gmail.com>
2018-10-16 16:03:57 -0500

    outlaw class/command mostly done.

---
+++ src/classes/outlaw.js
@@ -0,0 +1,15 @@
+module.exports = class Outlaw {
+    constructor(author, nickname, desc) {
+        this.author = author.id;
+        this.nickname = nickname;
+        this.desc = desc;
+        this.kills = 0;
+        this.bounty = 0; //TODO
+    }
+    displayInfo(msg) {
+        //send message of their outlaw info
+        msg.channel.send(`${msg.author}\n${this.nickname} Wanted Dead or Alive\n
+                            For ${this.kills} kills
+                            $$$$ ${this.boundy} $$$$`)
+    }
+}
--- src/commands/fun/duel.js
+++ src/commands/fun/duel.js
@@ -1,18 +1,8 @@
 'use strict';
 var commando = require("discord.js-commando");
-
-class Outlaw {
-    constructor(author) {
-        this.author = author;
-        this.kills = 0;
-        this.desc = "";
-        this.rapSheet = [];
-        this.bounty = 0;
-    }
-    displayInfo(msg) {
-        //send message of their outlaw info 
-    }
-}
+var Outlaw = require('../../classes/outlaw');
+var outlawStorage = "./../../../data/outlaw";
+var fs = require('fs');
 
 class Duel {
     constructor(msg, author, target){
@@ -22,8 +12,36 @@ class Duel {
         this.status = 'STARTED'
     }
 
-    calcWinner() {
+    calcWinner(msg) {
+        var randRoll1 = Math.floor(Math.random() * (this.author.kills + 100));
+        var randRoll2 = Math.floor(Math.random() * (this.target.kills + 100));
+        if (randRoll1 > randRoll2) {
+            this.author.kills += 1;
+            msg.channel.send(`:gun: ${msg.author} perished at the hands of ${args.target} :gun:`);
+        } else if(randRoll1 < randRoll2) {
+            this.target.kills += 1;            
+            msg.channel.send(`:gun: ${args.target} perished at the hands of ${msg.author} :gun:`);
+        } else {
+            msg.channel.send(`:gun: Both ${msg.author} and ${args.target} perished :gun:`);
+        }
+    }
 
+    storeResults() {
+        fs.readFile(outlawStorage, 'utf8', function readFileCallback(err, data) {
+            if(err) this.msg.channel.send('error retreving data, try again later Noo:b:');
+            else {
+                var obj = JSON.parse(data);
+                obj.outlaws.forEach(outlaw => {
+                    if(outlaw.author === this.author.author) {
+                        obj.outlaws[outlaw].kill = this.author.kills;
+                    } else if (outlaw.author === this.target.author) {
+                        obj.outlaws[outlaw].kill = this.target.kills;
+                    }
+                }); 
+                json = JSON.stringify(obj);
+                fs.writeFile(outlawStorage, json, 'utf8', callback)
+            }
+        })
     }
 }
 
@@ -51,19 +69,9 @@ module.exports = class DuelCMD extends commando.Command {
         if(msg.author === args.target) {
             msg.channel.send(`Silly ${msg.author} you can't duel yourself.`)
         } else {
-            var randRoll1 = Math.floor(Math.random() * 100);
-            var randRoll2 = Math.floor(Math.random() * 100);
-            if (randRoll1 > randRoll2) {
-                msg.channel.send(`:gun: ${msg.author} perished at the hands of ${args.target} :gun:`);
-            } else if(randRoll1 < randRoll2) {
-                msg.channel.send(`:gun: ${args.target} perished at the hands of ${msg.author} :gun:`);
-            } else {
-                msg.channel.send(`:gun: Both ${msg.author} and ${args.target} perished :gun:`);
-            }
+            var newDuel = new Duel(msg, authorOL, targetOL);
+            newDuel.calcWinner(msg);
+            newDuel.storeResults();
         }
-    }
-
-    startDuel(msg, author, target) {
-
     }
 }
