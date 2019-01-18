57dd4802bd01b8f719b3161f635086f6de19198e
Mitchell Dzurisin <madlab96@gmail.com>
2019-01-18 11:20:41 -0600

    fixed bug with mtg flip cards

--- src/util/mtgCard.js
+++ src/util/mtgCard.js
@@ -1,11 +1,21 @@
 const {RichEmbed} = require('discord.js');
 
 module.exports = function embedMessage(cardObj) {
-    const embed = new RichEmbed()
-        .setTitle(cardObj.name)
-        .setColor(0x00FF55)
-        .setImage(cardObj.image_uris.normal)
-        .addField('Est. Price', `$${cardObj.usd}`)
-        .addField('Link', `[scryfall](${cardObj.scryfall_uri})`);
-    return embed;
+    if(cardObj.card_faces) {
+        const embed = new RichEmbed()
+            .setTitle(cardObj.card_faces[0].name + " (Flip Card)")
+            .setColor(0x00FF55)
+            .setImage(cardObj.card_faces[0].image_uris.normal)
+            .addField('Est. Price', `$${cardObj.usd}`)
+            .addField('Link', `[scryfall](${cardObj.scryfall_uri})`);
+        return embed;
+    } else {
+        const embed = new RichEmbed()
+            .setTitle(cardObj.name)
+            .setColor(0x00FF55)
+            .setImage(cardObj.image_uris.normal)
+            .addField('Est. Price', `$${cardObj.usd}`)
+            .addField('Link', `[scryfall](${cardObj.scryfall_uri})`);
+        return embed;
+    }
 }
