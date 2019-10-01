8a07d7d3ea6e0565ec980d3d857a8a00eb446777
Mitchell Dzurisin <madlab96@gmail.com>
2019-10-01 15:20:56 -0400

    added TS

--- src/bot.ts
+++ src/bot.ts
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 'use strict';
 
-var auth = require('./auth.json');
-var ClientSystem = require('./src/ClientSystem');
+var auth = require('../auth.json');
+var ClientSystem = require('./ClientSystem');
 
 var cs = new ClientSystem(auth);
 
