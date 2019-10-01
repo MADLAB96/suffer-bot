5dba67c3222142c5794e4a94111752e2c5246f24
Mitchell Dzurisin <madlab96@gmail.com>
2019-10-01 14:15:51 -0400

    mroeasdfasdfasdfasdfaslkdfjasd;fa

--- bot.js
+++ bot.js
@@ -2,62 +2,9 @@
 'use strict';
 
 var auth = require('./auth.json');
-var commando = require('discord.js-commando');
-var path = require('path');
-var tmi  = require('tmi.js');
+var ClientSystem = require('./src/ClientSystem');
 
-const client = new commando.Client({
-    owner: '209463572395196417',
-    commandPrefix: '!'
-});
+var cs = new ClientSystem(auth);
 
-const twitchOpts = {
-    identity: {
-        username: 'suffer-bot',
-        password: auth.twitch
-    },
-
-    channels: [
-        'Madlab96'
-    ]
-}
-
-const twitchClient = new tmi.client(twitchOpts);
-
-twitchClient.connect();
-
-twitchClient.on('message', (target, context, msg, self) => {
-    console.log('new msg:', msg);
-    if(msg.trim().slice(0, 1) === '!') {
-        console.log('prefix !::');
-    }
-    if(msg.includes('society') || msg.includes('joker') || msg.includes('we')) {
-        twitchClient.say(target, 'gamer word');
-    }
-    if(msg.includes('nagger')) {
-        twitchClient.say(target, 'ayy');
-    }
-});
-
-
-twitchClient.on('connected', (addr, port) => {
-    console.log(`* Connected to ${addr}:${port}`);
-});
-
-client.on('ready', function (evt) {
-    var list = [];
-    client.channels.forEach(channel => {
-        list.push(channel.name)
-    });
-    console.log('connected to:', list);
-    console.log('Started');
-});
-
-client.registry
-        .registerGroup('fun', 'Fun')
-        .registerGroup('response', 'Response')
-        .registerDefaults()
-        .registerCommandsIn(path.join(__dirname, './src/commands'));
-
-// authentication
-client.login(auth.token);
+cs.initTwitch();
+cs.initDiscord();
--- src/ClientSystem.js
+++ src/ClientSystem.js
@@ -1,9 +1,82 @@
-export class ClientSystem {
-    constructor() {
+var commando = require('discord.js-commando');
+var path = require('path');
+var tmi  = require('tmi.js');
+
+module.exports = class ClientSystem {
+    constructor(auth) {
         this.discordClient = null;
         this.twitchClient = null;   
         this.commands = [];         
         this.defaultResponses = []; // Default responses
         this.storedRessponses = []; // responses stored in database
+        
+        if(auth) this.auth = auth;
+        else this.auth = {};
+    }
+
+    initDiscord() {
+        const client = new commando.Client({
+            owner: '209463572395196417',
+            commandPrefix: '!'
+        });
+        
+        client.on('ready', function (evt) {
+            var list = [];
+            client.channels.forEach(channel => {
+                list.push(channel.name)
+            });
+            console.log('connected to:', list);
+            console.log('Started');
+        });
+        
+        client.registry
+                .registerGroup('fun', 'Fun')
+                .registerGroup('response', 'Response')
+                .registerDefaults()
+                .registerCommandsIn(path.join(__dirname, './commands'));
+        
+        // authentication
+        client.login(this.auth.token);
+
+        return this;
+    }
+
+    initTwitch() {
+
+        // TODO: Import this from config file.
+        const twitchOpts = {
+            identity: {
+                username: 'suffer-bot',
+                password: this.auth.twitch
+            },
+        
+            channels: [
+                'Madlab96'
+            ]
+        }
+        
+        const twitchClient = new tmi.client(twitchOpts);
+        
+        twitchClient.connect();
+        
+        twitchClient.on('message', (target, context, msg, self) => {
+            console.log('new msg:', msg);
+            if(msg.trim().slice(0, 1) === '!') {
+                console.log('prefix !::');
+            }
+            if(msg.includes('society') || msg.includes('joker') || msg.includes('we')) {
+                twitchClient.say(target, 'gamer word');
+            }
+            if(msg.includes('nagger')) {
+                twitchClient.say(target, 'ayy');
+            }
+        });
+        
+        
+        twitchClient.on('connected', (addr, port) => {
+            console.log(`* Connected to ${addr}:${port}`);
+        });        
+
+        return this;
     }
 }
